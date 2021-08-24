0a3ace513986258c712fe51bd35dad0750e2800b
Thales TCVCB. Caique Viana Cereja Baeza <thalesbaeza@gac.local>
2021-08-24 13:38:29 -0300

    controle de estoque

--- app/index.py
+++ app/index.py
@@ -12,15 +12,15 @@ print("*************************")
 
 while True:
     escolha =  input("1-) DEMONSTRATIVO DE FLUXO DE CAIXA \n2-) CONTROLE DE ESTOQUE \n \n Qual é a opção desejada: ")
-    if escolha == '1': #DEMONSTRATIVO DE FLUXO DE CAIXA
+    if escolha == '1': 
         break
-    elif escolha == '2': #CONTROLE DE ESTOQUE
+    elif escolha == '2': 
         break
     else:
         True
 
 
-if escolha == '1':
+if escolha == '1': #DEMONSTRATIVO DE FLUXO DE CAIXA
     modelo = input("Inserir o modelo do Carro: ")
     
     data_inicial = datetime.today()
@@ -36,9 +36,7 @@ if escolha == '1':
         modelo = input("Inserir o modelo do Carro: ")
         
     valorVenda = input("Inserir o valor de venda: ")
-
     
-        
     print("*******************************")
     print("DEMONSTRATIVO DE FLUXO DE CAIXA")
     print("*******************************")
@@ -59,23 +57,45 @@ if escolha == '1':
     print("LUCRO : ", lucro)
     print("*******************************")
 
-    
-
     data_final = datetime.today()
-
-    con = sqlite3.connect('oficina.db')
-
-    cur = con.cursor()
-
-    query = '''CREATE TABLE IF NOT EXISTS pedidos (modelo_carro text, data_inicial datetime, data_final datetime, vls_compra real, vls_total_pecas real , vls_lucro real )'''
-
-    query2 = '''INSERT INTO pedidos (modelo_carro, data_inicial, data_final, vls_compra, vls_total_pecas, vls_lucro ) VALUES (?, ?, ?, ?, ?, ?)'''
 
     data = [modelo,data_inicial,data_final, valorIncial, valores, lucro]
 
-    cur.execute(query2, data)
+    con = sqlite3.connect('oficina_car.db')
+
+    cur = con.cursor()
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS pedidos (id INTEGER PRIMARY KEY AUTOINCREMENT, modelo_carro TEXT, data_inicial DATETIME, data_final DATETIME, vls_compra REAL, vls_total_pecas REAL , vls_lucro REAL )''')
+    cur.execute('''INSERT INTO pedidos (modelo_carro, data_inicial, data_final, vls_compra, vls_total_pecas, vls_lucro ) VALUES (?, ?, ?, ?, ?, ?)''', data)
+
     con.commit()
     con.close()
+
+elif escolha == '2': #DEMONSTRATIVO DE FLUXO DE CAIXA
+    escolhaEstoque =  input("1-) Inserir peça no estoque  \n2-) Retirar peça do estoque \n3-) Visualizar o Estoque \n\n Qual é a opção desejada: ")
+    con = sqlite3.connect('oficina_car.db')
+
+    cur = con.cursor()
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS Estoque (id INTEGER PRIMARY KEY AUTOINCREMENT, modelo_peca TEXT, data DATETIME, Qtd int )''')
+
+    con.commit()
+    con.close()
+    if escolhaEstoque == '1':
+        print("Inserir peça no estoque")
+    elif escolhaEstoque == '2':
+        print("Retirar peça do estoque")
+    else:
+        print("Mostrar o estoque atual")
+else:
+    sair = input("Deseja sair? (y/n)")
+    if sair.lower() == 'y':
+        print("Exit")
+    else:
+        print("Precisa voltar para o menu") 
+
+
+
 
 
 
